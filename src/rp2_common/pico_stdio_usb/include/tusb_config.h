7344b08c3f8ad34efcd7895b84c676abcf6feaf5
Tom Gao <zimingg2001@gmail.com>
2023-08-10 21:57:30 -0400

    Increased buffer so main loop publishes thru USB at >1kHz at least

--- src/rp2_common/pico_stdio_usb/include/tusb_config.h
+++ src/rp2_common/pico_stdio_usb/include/tusb_config.h
@@ -33,8 +33,9 @@
 #define CFG_TUSB_RHPORT0_MODE   (OPT_MODE_DEVICE)
 
 #define CFG_TUD_CDC             (2)
-#define CFG_TUD_CDC_RX_BUFSIZE  (256)
-#define CFG_TUD_CDC_TX_BUFSIZE  (256)
+#define CFG_TUD_CDC_RX_BUFSIZE  (512)
+#define CFG_TUD_CDC_TX_BUFSIZE  (512)
+#define CFG_TUD_CDC_EP_BUFSIZE (512)
 
 // We use a vendor specific interface but with our own driver
 #define CFG_TUD_VENDOR            (0)
